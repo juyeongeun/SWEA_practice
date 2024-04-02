e6f724f5388376964fa5eb7f89eb712d8f141cc6
juyeongeun <juye981203@naver.com>
2024-04-02 18:53:41 +0900

    신문 헤드라인

---
+++ 신문헤드라인/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String head = sc.next();
+
+        head=head.toUpperCase();
+        System.out.println(head);
+    }
+}
