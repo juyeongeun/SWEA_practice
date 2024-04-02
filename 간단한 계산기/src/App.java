052269c2818bb2d8b70502d879af0a8b1d831dbe
juyeongeun <juye981203@naver.com>
2024-04-02 18:36:08 +0900

    1938

--- 간단한 계산기/src/App.java
+++ 간단한 계산기/src/App.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = sc.nextInt(); //자연수 a
+        int b = sc.nextInt(); //자연수 b
 
+        //연산결과 출력
         System.out.println(a + b);
         System.out.println(a - b);
         System.out.println(a * b);
