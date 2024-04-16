9ce5d17cb3409e2ed96ae164647c534b4b75910b
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-04-16 22:28:05 +0900

    Update App.java

--- 백만 장자 프로젝트/src/App.java
+++ 백만 장자 프로젝트/src/App.java
@@ -21,23 +21,23 @@ public class App {
             }
 
             while (idx < cnt) {
-                idx_max = idx;
+                idx_max = idx; //최대값의 인덱스 = 현재 인덱스(수정된 인덱스 값)
             
-                // 최대값 구하기
+                // 최대값 구하기 i는 현재 인덱스부터
                 for (int i = idx; i < cnt; i++) {
                     if (price[idx_max] < price[i]) {
-                        idx_max = i;
+                        idx_max = i; // 최대값 인덱스는 i로 변경
                     }
                 }
 
-                // 구매
+                // 구매 : 최대값 전까지 구매함
                 for (int j = idx; j < idx_max; j++) {
                     money -= price[j];
                 }
 
-                // 판매
+                // 판매 : 최대값에서 구매한 개수만큼 더하기
                 money += price[idx_max] * (idx_max - idx);
-                idx = idx_max + 1;
+                idx = idx_max + 1; // 최대값 인덱스의 다음번째로 현재 인덱스값 수정
             }
             System.out.println("#" + tc + " " + money);
         }
