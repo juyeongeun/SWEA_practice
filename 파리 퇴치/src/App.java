82dc931562af457c10dd2db6adbd2f4fd7a9506b
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-04-16 22:41:52 +0900

    Update App.java

--- 파리 퇴치/src/App.java
+++ 파리 퇴치/src/App.java
@@ -7,26 +7,30 @@ public class App {
         int T = sc.nextInt();
 
         for (int TC = 1; TC <= T; TC++) {
-            int max = 0;
-            int sum = 0;
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-            int[][] bugs = new int[N][N];
+            int max = 0; // 최대값
+            int sum = 0; // 파리채 크기의 합
+            int N = sc.nextInt(); // 파리 배열판 크기
+            int M = sc.nextInt(); // 파리채 배열판 크기
+            int[][] bugs = new int[N][N]; // 파리 배열판
 
+            // 각 칸의 파리 수 입력
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     bugs[i][j] = sc.nextInt();
                 }
             }
 
+            // N-M : 인덱스 범위 오류 막기 위해서 파리채 크기만큼 뺌
             for (int i = 0; i <= N - M; i++) {
                 for (int j = 0; j <= N - M; j++) {
                     sum = 0;
                     for (int k = 0; k < M; k++) {
                         for (int r = 0; r < M; r++) {
+                            // i+k, j+r : 파리 배열판 시작지점+파리채 크기
                             sum += bugs[i + k][j + r];
                         }
                     }
+                    //최대값 구하기
                     if (sum >= max) {
                         max = sum;
                     }
