70afe3c37b922a7ba4926fd545ff63f59dafc4d0
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-04-16 22:47:20 +0900

    Update App.java

--- 파스칼의 삼각형/src/App.java
+++ 파스칼의 삼각형/src/App.java
@@ -9,16 +9,16 @@ public class App {
 
         for (int TC = 1; TC <= T; TC++) {
             int N = sc.nextInt();
-            arr = new int[N][N];
+            arr = new int[N][N]; // 파스칼 삼각형 층 수
             int num=1;
 
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (j == 0) {
+                    if (j == 0) { // 파스칼 시작 지점은 1
                         arr[i][j] = 1;
-                    } else if (j == N-1) {
+                    } else if (j == N-1) { // 파스칼 마지막 지점도 1
                         arr[i][j] = 1;
-                    } else if(i!=0) {
+                    } else if(i!=0) { // 현재 위치의 값 = 이전 행 왼쪽 값 + 이전 행 현재 열 값
                         arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
                     }
                 }
@@ -26,6 +26,7 @@ public class App {
 
             System.out.println("#"+TC);
 
+            // 삼각형의 층 수 만큼의 개수만 출력 ex) 0행 : 1개, 1행 : 2개 … n행 : n+1개
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < num; j++) {
                     System.out.print(arr[i][j] + " ");
