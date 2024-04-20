05a3d4a452294bcb93e9ff0b0c51c037ad8b4848
juyeongeun <juye981203@naver.com>
2024-04-20 23:25:26 +0900

    swea 거듭 제곱

---
+++ sw 문제해결 기본 4일차 - 거듭 제곱/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Solution {
+	public static int result;
+	public static int cnt;
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int T= sc.nextInt();
+			int N=sc.nextInt();
+			int M=sc.nextInt();
+			result=1;
+			cnt=0;
+
+			pows(N,M); // 함수 호출
+			System.out.println("#"+test_case+" "+result);
+		}
+	}
+	public static void pows(int n, int m){
+		if(m==0){ //m번의 곱셈이 끝나면
+			return;
+		}
+		result*=n;
+		pows(n,m-1); //재귀함수
+	}
+}
