fb8eb7cd477dcbd727e5a6b7cf883d2e4664ba2f
juyeongeun <juye981203@naver.com>
2024-05-07 19:37:44 +0900

    swea 단순 2진 암호문

--- sw 문제해결 응용 1일차 - 단순 2진 암호코드/src/App.java
+++ sw 문제해결 응용 1일차 - 단순 2진 암호코드/src/App.java
@@ -1,118 +1,61 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
-public class App {
+class Solution {
+	public static String[] code = { "0001101", "0011001", "0010011", "0111101", 
+			"0100011", "0110001", "0101111", "0111011", "0110111", "0001011"};
+	
+	public static int[] toNum(String s) {
+		int num[] = new int[8];
+		int idx = 0;
+		for (int i = 0; i < s.length(); i += 7) { 
+			String tmp = s.substring(i, i + 7); // 7개 뽑아오기
+			for (int j = 0; j < code.length; j++) { // 코드 배열에서 일치하는 것 찾기
+				if (tmp.equals(code[j])) {
+					num[idx] = j; // num배열에 숫자 넣기
+                    idx++; //idx 증가 시켜주기
+					break;
+				}
+			}
+		}
+		return num;
+	}
 
-    static String[][] passwords = {
-            { "0", "0", "0", "1", "1", "0", "1" },
-            { "0", "0", "1", "1", "0", "0", "1" },
-            { "0", "0", "1", "0", "0", "1", "1" },
-            { "0", "1", "1", "1", "1", "0", "1" },
-            { "0", "1", "0", "0", "0", "1", "1" },
-            { "0", "1", "1", "0", "0", "0", "1" },
-            { "0", "1", "0", "1", "1", "1", "1" },
-            { "0", "1", "1", "1", "0", "1", "1" },
-            { "0", "1", "1", "0", "1", "1", "1" },
-            { "0", "0", "0", "1", "0", "1", "1" }
-    };
+	public static int isValid(String code) {
+		int num[] = toNum(code);
+		int sum = 0;
+		int total = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
+		for (int i = 0; i < num.length; i++) {
+			sum += num[i]; // 총 합계 계산해주기
+			if (i % 2 == 0) total += 3 * num[i]; // 짝수라면 3*num[i] 해주기
+			else total += num[i]; // 아니면 total+num[i]
+		}
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
+		if (total % 10 == 0) // 계산한 수가 10의 배수라면 sum 출력
+			return sum;
+		return 0; // 아니면 0
+	}
 
-            String[][] lines = new String[n][m];
-            for (int i = 0; i < n; i++) {
-                lines[i] = sc.next().split("");
-            }
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
 
-            List<String> secretCode = new ArrayList<>(findSecretCode(lines));
-
-            for (int i = secretCode.size() - 1; i >= 0; i--) {
-                if (secretCode.get(i).equals("1")) {
-                    break;
-                }
-                secretCode.remove(secretCode.size() - 1);
-            }
-
-            List<Integer> passwordCodes = new ArrayList<>(findPasswordCodes(secretCode));
-
-            Collections.reverse(passwordCodes);
-
-            System.out.println("#" + test_case + " " + defineIsRight(passwordCodes));
-        }
-
-        sc.close();
-    }
-
-    static List<String> findSecretCode(String[][] lines) {
-        for (String[] line : lines) {
-            boolean check = false;
-
-            for (String code : line) {
-                if (code.equals("1")) {
-                    check = true;
-                    break;
-                }
-            }
-
-            if (check) {
-                return Arrays.asList(line);
-            }
-        }
-
-        return new ArrayList<>();
-    }
-
-    static List<Integer> findPasswordCodes(List<String> secretCode) {
-        List<Integer> passwordCodes = new ArrayList<>();
-        int i = secretCode.size() - 1;
-        while (i >= 0) {
-            List<String> temp = new ArrayList<String>();
-            for (int j = 0; j < 7; j++) {
-                temp.add(secretCode.get(i));
-                i--;
-            }
-
-            Collections.reverse(temp);
-
-            for (int k = 0; k < 10; k++) {
-                if (Arrays.equals(passwords[k], temp.toArray())) {
-                    passwordCodes.add(k);
-                    break;
-                }
-            }
-
-            if (secretCode.get(i).equals("0")) {
-                break;
-            }
-        }
-
-        return passwordCodes;
-    }
-
-    static int defineIsRight(List<Integer> passwords) {
-        int odd = 0;
-        int even = 0;
-        for (int i = 1; i < 9; i++) {
-            if (i % 2 == 1) {
-                odd += passwords.get(i - 1);
-            } else {
-                even += passwords.get(i - 1);
-            }
-        }
-
-        if ((odd * 3 + even) % 10 == 0) {
-            return odd + even;
-        }
-
-        return 0;
-    }
+			String code = "";
+			for (int i = 0; i < n; i++) {
+				String s = sc.next();
+				if (code.isEmpty()) {
+					for (int j = m - 1; j >= 0; j--) {
+						if (s.charAt(j) == '1') {
+							code = s.substring(j - 55, j + 1); // 암호코드만 뽑아서 code에 저장하기
+							break;
+						}
+					}
+				}
+			}
+			System.out.println("#" + test_case + " " + isValid(code));
+		}
+	}
 }
