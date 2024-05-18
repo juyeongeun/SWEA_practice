d9dc08150b6ab15f9417de43cde5035699724830
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-05-18 23:32:11 +0900

    최대 조작 횟수

--- swea_17642/src/swea_17642/Solution.java
+++ swea_17642/src/swea_17642/Solution.java
@@ -21,7 +21,8 @@ public class Solution {
 
 			if (result == 0) {
 				System.out.println("#" + TC + " 0");
-			} else if (result < 2) {
+			} else if (result < 2) { // 소수는 1과 자기자신으로만 나누어지는 수라서 제일 작은 수가 2임
+				// 두 수의 차가 2보다 작으면 소수 계산을 할 수 없기 때문에 -1
 				System.out.println("#" + TC + " -1");
 			} else {
 				System.out.println("#" + TC + " " + (long) (result / 2));
